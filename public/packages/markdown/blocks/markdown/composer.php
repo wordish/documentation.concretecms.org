2e42db693cce1db02fa974ab9b5f369ec68d288b
Andrew Embler <andrew@concrete5.org>
2023-01-18 07:38:55 -0800

    moving markdown into the repo because installing it via this stuff is annoying and not deploying updates for fixing CS-890

---
+++ public/packages/markdown/blocks/markdown/composer.php
@@ -0,0 +1,20 @@
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+$content = $content ?? null;
+?>
+
+<div class="control-group">
+	<label class="control-label"><?=$label?></label>
+	<?php if($description): ?>
+	<i class="fa fa-question-circle launch-tooltip" title="" data-original-title="<?=$description?>"></i>
+	<?php endif; ?>
+	<div class="controls">
+		<?php
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$data = $view->getRequestValue();
+			$content = $data['content'];
+		}
+		$view->inc('form.php', array('composerContent' => $content, 'fieldName' => $view->field('content')));
+		?>
+	</div>
+</div>
