0a7c472bcc7600da4636ad69512ef5f1ed717978
Korvin Szanto <Korvinszanto@gmail.com>
2023-04-05 13:14:19 -0700

    Use deploy manager for deployment

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-tempdir=/tmp/documentation/codedeployupload
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+source ../.codedeployvars
+tempdir=/tmp/codedeploy/codedeployupload/$jobid
 
 [[ -d $tempdir ]] && rm -r $tempdir
 mkdir -p $tempdir
@@ -9,13 +12,3 @@ mkdir -p $tempdir
 mkdir /tmp/$DEPLOYMENT_ID
 
 echo "export tempdir=\"$tempdir\"" > "/tmp/$DEPLOYMENT_ID/.cdvariables";
-
-if [ "$APPLICATION_NAME" == "documentation.stage.concretecms.org" ]
-then
-  echo "export projectdir=\"/home/forge/documentation.stage.concretecms.org\"" >> "/tmp/$DEPLOYMENT_ID/.cdvariables";
-  echo "export deploydir=\"/home/forge/documentation.stage.concretecms.org/releases/$DEPLOYMENT_ID\"" >> "/tmp/$DEPLOYMENT_ID/.cdvariables";
-elif [ "$APPLICATION_NAME" == "documentation.concretecms.org" ]
-then
-  echo "export projectdir=\"/home/forge/documentation.concretecms.org\"" >> "/tmp/$DEPLOYMENT_ID/.cdvariables";
-  echo "export deploydir=\"/home/forge/documentation.concretecms.org/releases/$DEPLOYMENT_ID\"" >> "/tmp/$DEPLOYMENT_ID/.cdvariables";
-fi
