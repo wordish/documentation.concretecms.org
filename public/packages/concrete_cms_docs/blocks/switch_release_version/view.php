ce88523353fbf0765c0de14a8c7f2c8a6544ceac
myq <myq@portlandlabs.com>
2024-01-25 15:35:34 -0800

    Update for release switcher in theme; add nag

--- public/packages/concrete_cms_docs/blocks/switch_release_version/view.php
+++ public/packages/concrete_cms_docs/blocks/switch_release_version/view.php
@@ -16,8 +16,8 @@ defined('C5_EXECUTE') or die('Access Denied.');
                 $releaseLabels,
                 $selectedRelease,
                 [
-                    'data-select' => 'multilingual-switch-language', // reuse existing JS in js/features/multilingual/frontend.js for switch language block
-                    'data-action' => $detector->getSwitchLink($cID, '--language--')
+                    'data-select' => 'switch-release-version',
+                    'data-action' => $detector->getSwitchLink($cID, '--release--')
                 ]
             ) ?>
         </div>
@@ -25,4 +25,10 @@ defined('C5_EXECUTE') or die('Access Denied.');
     <script>
      (()=>{var e={7986:()=>{$((function(){$("select[data-select=multilingual-switch-language]").change((function(){var e=$(this).attr("data-action").replace("--language--",$(this).val());window.location.href=e}))}))}},t={};function r(a){var n=t[a];if(void 0!==n)return n.exports;var o=t[a]={exports:{}};return e[a](o,o.exports,r),o.exports}r.n=e=>{var t=e&&e.__esModule?()=>e.default:()=>e;return r.d(t,{a:t}),t},r.d=(e,t)=>{for(var a in t)r.o(t,a)&&!r.o(e,a)&&Object.defineProperty(e,a,{enumerable:!0,get:t[a]})},r.o=(e,t)=>Object.prototype.hasOwnProperty.call(e,t),(()=>{"use strict";r(7986)})()})();
     </script>
+</div>
+<div class="alert alert-warning">
+    <aside>
+        <i class="fas fa-exclamation-triangle"></i>
+        This documentation is not for the most recent release version of Concrete CMS.
+    </aside>
 </div>
